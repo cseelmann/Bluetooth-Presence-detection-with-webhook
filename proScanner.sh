00a35be00c21b7c109e9ee1e4004d33dc4062f7f
mavnezz <julian@stuch.info>
2017-07-24 16:23:22 +0200

    Update proScanner.sh

--- proScanner.sh
+++ proScanner.sh
@@ -3,48 +3,61 @@
 # 24.07.2017
 # Comment:
 # Script läuft nach dem Start des RPI und Dauerschleife
-# wenn beide G-tags 5 Schleifendurchläufe nicht erkannt werden
+# wenn beide G-tags 15 Schleifendurchläufe nicht erkannt werden
 # wird auf Status Abwesend gesetzt
 
+# -------------------------
+# Einstellungen (edit here)
+# -------------------------
+away=15 	# nach wieviel Durchläufen Status "abwesend"?
+gtags=("7C:2F:80:90:22:22" "7C:2F:80:90:33:55")	# G-tags mac Adresses
+
+homeeip="homee.fritz.box"
+homeeport="7681"
+homeeport="80"
+webhooks_key="asdasdadas5d78asg4da6738"
+
+# ----------------------
+# do not edit below here 
+# ----------------------
+# Startverzögerung
+sleep 10
 ncounter=1
-daheim=2
+daheim=0
 
 # Whitelist clear
 sudo hcitool lewlclr
-
-# G-tags 2 Stück im Einsatz
-sudo hcitool lewladd "7C:2F:xx:xx:46:xx"
-sudo hcitool lewladd "7C:2F:xx:xx:46:yy"
+# G-tags zur Whitelist
+echo "Gültige G-tags"
+for i in ${gtags[@]}; do
+	echo "$i"
+	sudo hcitool lewladd "$i"
+done
+echo ""
 
 while true; do
-    sudo hcitool lescan --whitelist > scan.txt & sleep 3 && sudo pkill --signal SIGINT hcito   
+    sudo hcitool lescan --whitelist > scan.txt & sleep 2 && sudo pkill --signal SIGINT hcito   
     NUMOFLINES=$(wc -l < "scan.txt")
-    #echo $NUMOFLINES
     if [ "$NUMOFLINES" -gt "1" ]; then
-        while read -r line; do   
-    	    line=${line:0:17}
-    	    check=${line:0:7}
-    	    if [ "$check" != "LE Scan" ]; then
-    	        if [ "$line" == "7C:2F:xx:xx:46:xx" ] || [ "$line" == "7C:2F:xx:xx:46:yy" ]; then
-		    ncounter=1
-                    if [ "$daheim" == "0" ] || [ "$daheim" == "2" ]; then
-                        daheim=1
-                        echo $line "Anwesend"
-			# Hier URL Aufruf für Homee Webhook
-                    fi
-                fi
-    	    fi
-        done <scan.txt 
+		# Anwesend
+		if [ "$daheim" -eq 0 ]; then
+			echo "Status: anwesend"	
+			curl -sS "http://$homeeip:$homeeport/api/v2/webhook_trigger?webhooks_key=$webhooks_key&event=anwesend"	
+			daheim=1
+		fi
+		ncounter=1
     else
-        if [ "$daheim" == "1" ]; then
-	   echo "Counter Abwesend: " $ncounter
-	   if [ "$ncounter" == "5" ]; then
-               echo "Abwesend"
-	       # Hier URL Aufruf für Homee Webhook
-               daheim=0
-           fi
-	   ncounter=$[ncounter+ 1]
-        fi
+		# Abwesend
+		if [ "$ncounter" -lt "$away" ]; then
+			echo "Counter Abwesend: " $ncounter
+		fi
+		
+		if [ "$ncounter" == "$away" ]; then
+			echo "Status: abwesend"
+			curl -sS "http://$homeeip:$homeeport/api/v2/webhook_trigger?webhooks_key=$webhooks_key&event=abwesend"
+			daheim=0
+		fi
+		ncounter=$[ncounter+ 1]
     fi
-    sleep 2
+    sleep 1
 done
